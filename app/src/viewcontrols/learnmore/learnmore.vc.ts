62e4247de37ab5319b8571186510478c9fcfa275
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-10-19 17:05:50 -0500

    email register page partially done

--- app/src/viewcontrols/learnmore/learnmore.vc.ts
+++ app/src/viewcontrols/learnmore/learnmore.vc.ts
@@ -4,7 +4,15 @@ import BaseViewControl from '../base/base.vc';
 export default class LearnMoreViewControl extends BaseViewControl {
     templateString: string = require('./learnmore.vc.html');
 
-    context: any = {};
+    context: any = {
+		name: <string> 'NAME',
+		email: <string> 'EMAIL'
+	};
+
+	registerEmail = function() {
+		alert(this.context.name + "\n" + this.context.email);
+	}
+
 }
 
 register.viewControl('learnmore-vc', LearnMoreViewControl);
