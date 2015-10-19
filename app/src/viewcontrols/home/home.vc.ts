0a724dd654bdbf1b09b96aaa4c522e73a05d2765
[Michael] <michael@lewiscommunications.com>
2015-10-19 11:20:30 -0500

    added imports to home page

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -1,5 +1,8 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import aboutViewControl from '../../viewcontrols/about/about.vc';
+import connectViewControl from '../../viewcontrols/connect/connect.vc';
+import portfolioViewControl from '../../viewcontrols/portfolio/portfolio.vc'
 
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
