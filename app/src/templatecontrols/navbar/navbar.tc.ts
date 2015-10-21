0ba3011065d402e6f8f00eb8e8a4a4a14c4151c2
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-10-21 15:36:22 -0500

    jay commit

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -2,7 +2,26 @@ import {controls, events, register, ui, web} from 'platypus';
 import {DrawerController} from 'platypusui';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
-   templateString: string = require('./navbar.tc.html');
+
+	templateString: string = require('./navbar.tc.html');
+
+	drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
+	
+	context: any = {
+		showNavbar: true
+	};
+	
+	initialize(): void {
+		this.on('navigated', (ev: events.DispatchEvent, utils: web.UrlUtils) => {
+			this.drawerController.control.close();
+			// if(utils.pathname.indexOf('/login') === 0 ||
+			// 	utils.pathname.indexOf('/register') === 0) {
+			// 	this.context.showNavbar = false;
+			// } else {
+			// 	this.context.showNavbar = true;
+			// }
+		});
+	}
 }
 
 register.control('navbar', NavbarTemplateControl);
