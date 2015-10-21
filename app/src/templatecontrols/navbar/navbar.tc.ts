a4eb2c4f719b6406825853d1a784c561528820aa
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-10-21 09:41:13 -0500

    added michael's navbar

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -2,25 +2,34 @@ import {controls, events, register, ui, web} from 'platypus';
 import {DrawerController} from 'platypusui';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
-    templateString: string = require('./navbar.tc.html');
-
-	drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
-	
-	context: any = {
-		showNavbar: true
-	};
-	
-	initialize(): void {
-		this.on('navigated', (ev: events.DispatchEvent, utils: web.UrlUtils) => {
-			this.drawerController.control.close();
-			// if(utils.pathname.indexOf('/login') === 0 ||
-			// 	utils.pathname.indexOf('/register') === 0) {
-			// 	this.context.showNavbar = false;
-			// } else {
-			// 	this.context.showNavbar = true;
-			// }
-		});
-	}
+   templateString: string = require('./navbar.tc.html');
 }
 
 register.control('navbar', NavbarTemplateControl);
+
+// import {controls, events, register, ui, web} from 'platypus';
+// import {DrawerController} from 'platypusui';
+
+// export default class NavbarTemplateControl extends ui.TemplateControl {
+//     templateString: string = require('./navbar.tc.html');
+
+// 	drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
+	
+// 	context: any = {
+// 		showNavbar: true
+// 	};
+	
+// 	initialize(): void {
+// 		this.on('navigated', (ev: events.DispatchEvent, utils: web.UrlUtils) => {
+// 			this.drawerController.control.close();
+// 			// if(utils.pathname.indexOf('/login') === 0 ||
+// 			// 	utils.pathname.indexOf('/register') === 0) {
+// 			// 	this.context.showNavbar = false;
+// 			// } else {
+// 			// 	this.context.showNavbar = true;
+// 			// }
+// 		});
+// 	}
+// }
+
+// register.control('navbar', NavbarTemplateControl);
