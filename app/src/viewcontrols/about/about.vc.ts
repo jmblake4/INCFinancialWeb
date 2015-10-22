99a43922e7c2914925ea6d4db0ae86d282ab3dfa
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-10-22 16:55:38 -0500

    added about toggle functions

--- app/src/viewcontrols/about/about.vc.ts
+++ app/src/viewcontrols/about/about.vc.ts
@@ -8,7 +8,49 @@ import HomeTwoViewControl from '../../viewcontrols/hometwo/hometwo.vc';
 export default class AboutViewControl extends BaseViewControl {
     templateString: string = require('./about.vc.html');
 
-    context: any = {};
+    context: any = {
+		teamVisible: <boolean> true,
+		worksVisible: <boolean> false,
+		purchasesVisible: <boolean> false,
+		incomeVisible: <boolean> false,
+		wealthVisible: <boolean> false,
+		financialVisible: <boolean> false		
+	};
+	
+	teamShow(): void {
+		this.context.teamVisible = true;
+		this.context.worksVisible = false;
+		this.context.purchasesVisible = false;
+		this.context.incomeVisible = false;
+		this.context.wealthVisible = false;
+		this.context.financialVisible = false;
+	}
+	
+	workShow(): void {
+		this.context.teamVisible = false;
+		this.context.worksVisible = true;
+		this.context.purchasesVisible = false;
+		this.context.incomeVisible = false;
+		this.context.wealthVisible = false;
+		this.context.financialVisible = false;		
+	}
+
+	togglePurchases(): void {
+		this.context.purchasesVisible = ! this.context.purchasesVisible;
+	}
+
+	toggleIncome(): void {
+		this.context.incomeVisible = ! this.context.incomeVisible;
+	}
+
+	toggleWealth(): void {
+		this.context.wealthVisible = ! this.context.wealthVisible;
+	}
+
+	toggleFinancial(): void {
+		this.context.financialVisible = ! this.context.financialVisible;
+	}
+	
 }
 
 register.viewControl('about-vc', AboutViewControl);
