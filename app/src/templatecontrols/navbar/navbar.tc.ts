aaa30c36e816c23e065197c6975e110875f81aa6
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-10-24 13:36:37 -0500

    converted some dynamic img src links to plat-src to avoid 404 errors on page load

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -8,7 +8,7 @@ export default class NavbarTemplateControl extends ui.TemplateControl {
 	drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
 	
 	context: any = {
-		showNavbar: true
+		showNavbar: false
 	};
 	
 	initialize(): void {
@@ -18,37 +18,10 @@ export default class NavbarTemplateControl extends ui.TemplateControl {
 			// 	utils.pathname.indexOf('/register') === 0) {
 			// 	this.context.showNavbar = false;
 			// } else {
-			// 	this.context.showNavbar = true;
+				this.context.showNavbar = true;
 			// }
 		});
 	}
 }
 
 register.control('navbar', NavbarTemplateControl);
-
-// import {controls, events, register, ui, web} from 'platypus';
-// import {DrawerController} from 'platypusui';
-
-// export default class NavbarTemplateControl extends ui.TemplateControl {
-//     templateString: string = require('./navbar.tc.html');
-
-// 	drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
-	
-// 	context: any = {
-// 		showNavbar: true
-// 	};
-	
-// 	initialize(): void {
-// 		this.on('navigated', (ev: events.DispatchEvent, utils: web.UrlUtils) => {
-// 			this.drawerController.control.close();
-// 			// if(utils.pathname.indexOf('/login') === 0 ||
-// 			// 	utils.pathname.indexOf('/register') === 0) {
-// 			// 	this.context.showNavbar = false;
-// 			// } else {
-// 			// 	this.context.showNavbar = true;
-// 			// }
-// 		});
-// 	}
-// }
-
-// register.control('navbar', NavbarTemplateControl);
